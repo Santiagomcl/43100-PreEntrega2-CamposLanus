465f22bdd6df1fce3c4e0e60831aedd72126be75
Santiagomcl <santiagomcl2191@gmail.com>
2023-06-12 14:13:16 -0300

    Segunda Pre-Entrega

--- main.js
+++ main.js
@@ -1,115 +1,86 @@
-let numeroPasos = prompt("Ingrese el número de pasos:");
+let numeroPasos = 0;
 let contadorPasos = 0;
-let velocidad = prompt("Ingrese la velocidad (rápida/lenta):");
-let brazosArriba = confirm("¿Los brazos están Arriba?");
+let velocidad = "";
+let brazosArriba = false;
 let auxiliar = 0;
+let resultados = [];
 
+const pasosBallet = [
+  { nombre: "Plié", velocidad: "normal", brazos: "Primera" },
+  { nombre: "Grand Plié", velocidad: "lenta", brazos: "Quinta" },
+  { nombre: "Relevé", velocidad: "normal", brazos: "Primera" },
+  { nombre: "Pirueta", velocidad: "rápida", brazos: "Quinta" },
+  { nombre: "Passe", velocidad: "rápida", brazos: "Quinta" },
+  { nombre: "Tour", velocidad: "rápida", brazos: "Primera" },
+  { nombre: "Sissone", velocidad: "media", brazos: "Tercera" },
+];
 
-function plié() {
-  switch (velocidad) {
-    case "rápida":
-      console.log("Plié rápido");
-      break;
-    case "lenta":
-      console.log("Plié lento");
-      break;
-    default:
-      console.log("Grand Plié");
-      break;
-  }
-  alert("Realizando plié...");
+function ejecutarPaso(paso) {
+  console.log(paso);
+  alert(`Realizando ${paso.nombre}...\n${paso.nombre} ${paso.velocidad} con brazos en ${paso.brazos}`);
+  resultados.push(paso);
 }
 
+function seleccionarPasoAlAzar() {
+  const indiceAleatorio = Math.floor(Math.random() * pasosBallet.length);
+  return pasosBallet[indiceAleatorio];
+}
 
-function plié(velocidad, brazosArriba) {
-    let mensaje = "";
-    switch (velocidad) {
-      case "rápida":
-        mensaje = "Plié rápido";
-        break;
-      case "lenta":
-        mensaje = "Grand Plié";
-        break;
-      default:
-        mensaje = "Plié";
-        break;
-    }
-    if (brazosArriba) {
-      mensaje += " con brazos en Quinta";
-    } else {
-      mensaje += " con brazos en Primera";
-    }
-    console.log(mensaje);
-    alert(`Realizando plié...\n${mensaje}`);
-    return mensaje;
-  }
-  
-  function relevé(brazosArriba) {
-    let mensaje = "";
-    if (brazosArriba) {
-      mensaje = "Relevé con brazos en Quinta";
-    } else {
-      mensaje = "Relevé con brazos en Primera";
-    }
-    console.log(mensaje);
-    alert(`Realizando relevé...\n${mensaje}`);
-    return mensaje;
-  }
-  
+function mostrarVideoDelDia() {
+  const enlaceVideo = "https://youtu.be/5J9la3Dp9uU";
+  console.log("Video del día:", enlaceVideo);
+}
 
-  function pirueta(velocidad, brazosArriba) {
-    let mensaje = "";
-    switch (velocidad) {
-      case "rápida":
-        mensaje = "Pirueta rápida";
-        break;
-      case "lenta":
-        mensaje = "Pirueta lenta";
-        break;
-      default:
-        mensaje = "Pirueta";
-        break;
-    }
-    if (brazosArriba) {
-      mensaje += " con brazos en Quinta";
-    } else {
-      mensaje += " con brazos en Primera";
-    }
-    console.log(mensaje);
-    alert(`Realizando pirueta...\n${mensaje}`);
-    return mensaje;
-  }
-  
-  
+function realizarDanza() {
+  numeroPasos = parseInt(prompt("Ingrese el número de pasos:"));
+  velocidad = prompt("Ingrese la velocidad (normal/rápida/lenta):");
+  brazosArriba = confirm("¿Los brazos están arriba?");
 
+  contadorPasos = 0;
+  auxiliar = 0;
+  resultados = [];
 
-if (!isNaN(numeroPasos) && numeroPasos > 0) {
+  if (!isNaN(numeroPasos) && numeroPasos > 0) {
+    while (contadorPasos < numeroPasos) {
+      contadorPasos++;
 
-  while (contadorPasos < numeroPasos) {
-    contadorPasos++;
+      const pasoActual = pasosBallet[auxiliar % pasosBallet.length];
+      ejecutarPaso(pasoActual);
 
-    if (auxiliar % 3 === 0 && auxiliar % 5 === 0) {
-        pirueta(velocidad, brazosArriba);
-      } else if (auxiliar % 3 === 0) {
-        plié(velocidad, brazosArriba);
-      } else if (auxiliar % 5 === 0) {
-        relevé(brazosArriba);
-      } else {
-        console.log("Primer Port de Bras");
-        alert("Realizando Primer Port de Bras...");
+      auxiliar++;
+
+      if (auxiliar === 10) {
+        velocidad = "lenta";
       }
 
-    auxiliar++;
-
-    if (auxiliar === 10) {
-      velocidad = "lenta";
+      if (auxiliar === 15) {
+        brazosArriba = false;
+      }
     }
 
-    if (auxiliar === 15) {
-      brazosArriba = false;
+    console.log("Lista de resultados:");
+    for (let i = 0; i < resultados.length; i++) {
+      console.log(`- ${resultados[i].nombre} ${resultados[i].velocidad} con brazos en ${resultados[i].brazos}`);
     }
+
+    console.table(resultados);
+
+    const pasoPreferido = seleccionarPasoAlAzar();
+    console.log("Paso preferido del día:", pasoPreferido);
+
+    mostrarVideoDelDia();
+
+    alert("Danza completada. Revisa la consola para ver los resultados.");
+  } else {
+    console.log("El número de pasos ingresado no es válido.");
+    alert("El número de pasos ingresado no es válido.");
   }
-} else {
-  console.log("El número de pasos ingresado no es válido.");
-  alert("El número de pasos ingresado no es válido.");
 }
+
+function iniciarDanza() {
+  if (confirm("¿Deseas iniciar la danza?")) {
+    realizarDanza();
+  }
+}
+
+iniciarDanza();
