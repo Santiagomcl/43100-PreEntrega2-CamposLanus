52dcd7b185a75016f8136ae28cbd0763eab9ca46
Santiagomcl <santiagomcl2191@gmail.com>
2023-06-27 18:55:42 -0300

    3 pre-entrega

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const pasosBallet = [];
 
-// Obtener el formulario y los campos de entrada
+
 const loginForm = document.getElementById("loginForm");
 const usernameInput = document.getElementById("usernameInput");
 const pasosForm = document.getElementById("pasosForm");
@@ -8,70 +8,70 @@ const nombrePasoInput = document.getElementById("nombrePasoInput");
 const velocidadPasoInput = document.getElementById("velocidadPasoInput");
 const brazosPasoInput = document.getElementById("brazosPasoInput");
 
-// Obtener los usuarios logueados desde el almacenamiento local o inicializarlo como un array vacío
+
 let usuariosLogueados = JSON.parse(localStorage.getItem("usuariosLogueados")) || [];
 
-// Manejar el evento de envío del formulario de inicio de sesión
+
 loginForm.addEventListener("submit", function (event) {
-  event.preventDefault(); // Evitar el envío del formulario
+  event.preventDefault(); 
 
   const username = usernameInput.value;
 
-  // Crear un objeto de usuario logueado
+ 
   const usuarioLogueado = {
     username: username
   };
 
-  // Agregar el usuario logueado al array de usuariosLogueados
+  
   usuariosLogueados.push(usuarioLogueado);
 
-  // Limpiar el campo de entrada
+  
   usernameInput.value = "";
 
-  // Mostrar los usuarios logueados
+  
   mostrarUsuariosLogueados();
 
-  // Guardar los usuarios logueados en el localStorage
+  
   localStorage.setItem("usuariosLogueados", JSON.stringify(usuariosLogueados));
 });
 
-// Manejar el evento de envío del formulario de pasos de danza
+
 pasosForm.addEventListener("submit", function (event) {
-  event.preventDefault(); // Evitar el envío del formulario
+  event.preventDefault(); 
 
   const nombrePaso = nombrePasoInput.value;
   const velocidadPaso = velocidadPasoInput.value;
   const brazosPaso = brazosPasoInput.value;
 
-  // Crear un objeto de paso de danza
+ 
   const paso = {
     nombre: nombrePaso,
     velocidad: velocidadPaso,
     brazos: brazosPaso
   };
 
-  // Agregar el paso de danza al array de pasosBallet
+  
   pasosBallet.push(paso);
 
-  // Limpiar los campos de entrada
+ 
   nombrePasoInput.value = "";
   velocidadPasoInput.value = "";
   brazosPasoInput.value = "";
 
-  // Mostrar el resultado
+ 
   mostrarResultado();
 
-  // Mostrar el video correspondiente al paso de danza
+ 
   mostrarVideo(pasosBallet.length - 1);
 
-  // Guardar los pasos de danza en el localStorage
+ 
   localStorage.setItem("pasosBallet", JSON.stringify(pasosBallet));
 });
 
-// Función para mostrar los usuarios logueados
+
 function mostrarUsuariosLogueados() {
   const usuariosLogueadosUl = document.getElementById("usuariosLogueados");
-  usuariosLogueadosUl.innerHTML = ""; // Limpiar la lista de usuarios logueados
+  usuariosLogueadosUl.innerHTML = ""; 
 
   for (let i = 0; i < usuariosLogueados.length; i++) {
     const usuarioLogueado = usuariosLogueados[i];
@@ -81,10 +81,10 @@ function mostrarUsuariosLogueados() {
   }
 }
 
-// Función para mostrar el resultado de los pasos de danza
+
 function mostrarResultado() {
   const resultadoDiv = document.getElementById("resultado");
-  resultadoDiv.innerHTML = ""; // Limpiar el contenido previo
+  resultadoDiv.innerHTML = ""; 
 
   for (let i = 0; i < pasosBallet.length; i++) {
     const paso = pasosBallet[i];
@@ -94,10 +94,10 @@ function mostrarResultado() {
   }
 }
 
-// Función para mostrar el video correspondiente al paso de danza
+
 function mostrarVideo(index) {
   const videoContainer = document.getElementById("videoContainer");
-  videoContainer.innerHTML = ""; // Limpiar el contenido previo
+  videoContainer.innerHTML = ""; 
 
   const videoId = obtenerVideoId(index);
   const iframe = document.createElement("iframe");
@@ -111,27 +111,27 @@ function mostrarVideo(index) {
   videoContainer.appendChild(iframe);
 }
 
-// Función para obtener el ID del video de YouTube según el índice del paso
+
 function obtenerVideoId(index) {
   const videos = [
     "h_VE64GXjQQ",
     "jQ73d68HQCs",
     "vgeyctb6r50"
-    // Agrega más IDs de video según los pasos de la danza
+    
   ];
   return videos[index];
 }
 
-// Obtener los pasos de danza desde el almacenamiento local o inicializarlo como un array vacío
+
 pasosBallet = JSON.parse(localStorage.getItem("pasosBallet")) || [];
 
-// Mostrar los usuarios logueados
+
 mostrarUsuariosLogueados();
 
-// Mostrar el resultado de los pasos de danza
+
 mostrarResultado();
 
-// Mostrar el video del último paso de danza si existen pasos
+
 if (pasosBallet.length > 0) {
   mostrarVideo(pasosBallet.length - 1);
 }
