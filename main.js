d1d186f17812a96c95e95b5148edd275f6cffd54
Santiagomcl <santiagomcl2191@gmail.com>
2023-07-16 17:46:46 -0300

    trabjo final JavaScript

--- main.js
+++ main.js
@@ -1,137 +1,234 @@
-const pasosBallet = [];
+const performancesDanzaContemporanea = [];
+const devoluciones = [];
+const usuariosLogueados = [];
 
 
 const loginForm = document.getElementById("loginForm");
-const usernameInput = document.getElementById("usernameInput");
+const firstNameInput = document.getElementById("firstNameInput");
+const lastNameInput = document.getElementById("lastNameInput");
 const pasosForm = document.getElementById("pasosForm");
-const nombrePasoInput = document.getElementById("nombrePasoInput");
-const velocidadPasoInput = document.getElementById("velocidadPasoInput");
-const brazosPasoInput = document.getElementById("brazosPasoInput");
-
-
-let usuariosLogueados = JSON.parse(localStorage.getItem("usuariosLogueados")) || [];
+const nombrePerformanceInput = document.getElementById("nombrePerformanceInput");
+const youtubeLinkInput = document.getElementById("youtubeLinkInput");
+const btnDarDevolucion = document.getElementById("btnDarDevolucion");
+const devolucionesIcono = document.getElementById("devolucionesIcono");
+const welcomeMessage = document.getElementById("welcomeMessage");
 
 
 loginForm.addEventListener("submit", function (event) {
   event.preventDefault(); 
 
-  const username = usernameInput.value;
+  const firstName = firstNameInput.value;
+  const lastName = lastNameInput.value;
 
- 
-  const usuarioLogueado = {
-    username: username
-  };
 
-  
-  usuariosLogueados.push(usuarioLogueado);
+  localStorage.setItem("firstName", firstName);
+  localStorage.setItem("lastName", lastName);
 
-  
-  usernameInput.value = "";
 
-  
-  mostrarUsuariosLogueados();
-
-  
-  localStorage.setItem("usuariosLogueados", JSON.stringify(usuariosLogueados));
+  Swal.fire({
+    icon: "success",
+    title: "Inicio de sesión exitoso",
+    text: "¡Bienvenido/a!",
+    confirmButtonText: "Aceptar"
+  }).then((result) => {
+    if (result.isConfirmed) {
+      mostrarWelcomeMessage(firstName, lastName);
+    }
+  });
 });
+
+
+function mostrarWelcomeMessage(firstName, lastName) {
+  welcomeMessage.textContent = `Bienvenido/a, ${firstName} ${lastName}`;
+}
 
 
 pasosForm.addEventListener("submit", function (event) {
   event.preventDefault(); 
 
-  const nombrePaso = nombrePasoInput.value;
-  const velocidadPaso = velocidadPasoInput.value;
-  const brazosPaso = brazosPasoInput.value;
+  const nombrePerformance = nombrePerformanceInput.value;
+  const youtubeLink = youtubeLinkInput.value;
 
- 
-  const paso = {
-    nombre: nombrePaso,
-    velocidad: velocidadPaso,
-    brazos: brazosPaso
+  
+  const performance = {
+    nombre: nombrePerformance,
+    youtubeLink: youtubeLink
   };
 
   
-  pasosBallet.push(paso);
+  performancesDanzaContemporanea.push(performance);
 
- 
-  nombrePasoInput.value = "";
-  velocidadPasoInput.value = "";
-  brazosPasoInput.value = "";
+  
+  nombrePerformanceInput.value = "";
+  youtubeLinkInput.value = "";
 
- 
-  mostrarResultado();
+  
+  mostrarPerformance(performancesDanzaContemporanea.length - 1);
 
- 
-  mostrarVideo(pasosBallet.length - 1);
-
- 
-  localStorage.setItem("pasosBallet", JSON.stringify(pasosBallet));
+  
+  Swal.fire({
+    icon: "success",
+    title: "Performance agregada",
+    text: "La performance ha sido agregada exitosamente",
+    confirmButtonText: "Aceptar"
+  }).then((result) => {
+    if (result.isConfirmed) {
+      mostrarDevolutionButton();
+    }
+  });
 });
 
 
-function mostrarUsuariosLogueados() {
-  const usuariosLogueadosUl = document.getElementById("usuariosLogueados");
-  usuariosLogueadosUl.innerHTML = ""; 
-
-  for (let i = 0; i < usuariosLogueados.length; i++) {
-    const usuarioLogueado = usuariosLogueados[i];
-    const li = document.createElement("li");
-    li.textContent = usuarioLogueado.username;
-    usuariosLogueadosUl.appendChild(li);
-  }
+function mostrarDevolutionButton() {
+  btnDarDevolucion.classList.remove("hidden");
 }
 
 
-function mostrarResultado() {
+function mostrarPerformance(index) {
   const resultadoDiv = document.getElementById("resultado");
-  resultadoDiv.innerHTML = ""; 
+  const videoContainer = document.getElementById("videoContainer");
+  resultadoDiv.innerHTML = `Performance: ${performancesDanzaContemporanea[index].nombre}`;
 
-  for (let i = 0; i < pasosBallet.length; i++) {
-    const paso = pasosBallet[i];
-    const p = document.createElement("p");
-    p.textContent = `${paso.nombre} - ${paso.velocidad} - ${paso.brazos}`;
-    resultadoDiv.appendChild(p);
+
+  const videoLink = performancesDanzaContemporanea[index].youtubeLink;
+  if (videoLink) {
+    const videoId = obtenerVideoIdFromLink(videoLink);
+    const videoHtml = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+    videoContainer.innerHTML = videoHtml;
+  } else {
+    videoContainer.innerHTML = "";
   }
 }
 
 
-function mostrarVideo(index) {
-  const videoContainer = document.getElementById("videoContainer");
-  videoContainer.innerHTML = ""; 
+function obtenerVideoIdFromLink(youtubeLink) {
+  const regex = /(?:\?v=|\/embed\/|\.be\/|\/watch\?v=|\/v\/|youtu\.be\/|\/embed\/|\/v=|youtube\.com\/watch\?v=|youtube\.com\/v\/)([^#\&\?\n<>\'\" ]{11})/;
+  const match = youtubeLink.match(regex);
 
-  const videoId = obtenerVideoId(index);
-  const iframe = document.createElement("iframe");
-  iframe.width = "560";
-  iframe.height = "315";
-  iframe.src = `https://www.youtube.com/embed/${videoId}`;
-  iframe.frameborder = "0";
-  iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-  iframe.allowfullscreen = true;
+  if (match && match[1]) {
+    return match[1];
+  }
 
-  videoContainer.appendChild(iframe);
+  return null;
 }
 
 
-function obtenerVideoId(index) {
-  const videos = [
-    "h_VE64GXjQQ",
-    "jQ73d68HQCs",
-    "vgeyctb6r50"
-    
-  ];
-  return videos[index];
+actualizarContadorDevoluciones();
+
+
+devolucionesIcono.addEventListener("click", function () {
+  mostrarDevolucionesGuardadas();
+});
+
+
+function actualizarContadorDevoluciones() {
+  const contadorDevoluciones = document.getElementById("contadorDevoluciones");
+  contadorDevoluciones.innerText = devoluciones.length.toString();
 }
 
 
-pasosBallet = JSON.parse(localStorage.getItem("pasosBallet")) || [];
+function mostrarDevolucionesGuardadas() {
+  let html = "<ul>";
+  devoluciones.forEach((devolucion, index) => {
+    html += `<li>
+      Performance: ${devolucion.performance}<br>
+      Devolución: ${devolucion.texto}<br>
+      Video: ${devolucion.videoLink}<br>
+      Usuario: ${devolucion.userName}<br>
+      <button class="compartir-twitter" data-index="${index}">Compartir en Twitter</button>
+      <button class="compartir-facebook" data-index="${index}">Compartir en Facebook</button>
+      <button class="compartir-instagram" data-index="${index}">Compartir en Instagram</button>
+      </li>`;
+  });
+  html += "</ul>";
+
+  Swal.fire({
+    title: "Devoluciones guardadas",
+    html: html,
+    showConfirmButton: false,
+  });
 
 
-mostrarUsuariosLogueados();
+  document.querySelectorAll(".compartir-twitter").forEach((button) => {
+    button.addEventListener("click", function () {
+      const index = parseInt(button.getAttribute("data-index"));
+      compartirEnTwitter(devoluciones[index].videoLink);
+    });
+  });
+
+  document.querySelectorAll(".compartir-facebook").forEach((button) => {
+    button.addEventListener("click", function () {
+      const index = parseInt(button.getAttribute("data-index"));
+      compartirEnFacebook(devoluciones[index].videoLink);
+    });
+  });
+
+  document.querySelectorAll(".compartir-instagram").forEach((button) => {
+    button.addEventListener("click", function () {
+      const index = parseInt(button.getAttribute("data-index"));
+      compartirEnInstagram(devoluciones[index].videoLink);
+    });
+  });
+}
 
 
-mostrarResultado();
+function compartirEnTwitter(videoLink) {
+  const url = `https://twitter.com/intent/tweet?text=Mira esta increíble devolución de danza contemporánea: ${videoLink}`;
+  window.open(url, "_blank");
+}
 
 
-if (pasosBallet.length > 0) {
-  mostrarVideo(pasosBallet.length - 1);
+function compartirEnFacebook(videoLink) {
+  const url = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(videoLink)}`;
+  window.open(url, "_blank");
+}
+
+
+function compartirEnInstagram(videoLink) {
+  const url = `https://www.instagram.com/?url=${encodeURIComponent(videoLink)}`;
+  window.open(url, "_blank");
+}
+
+
+btnDarDevolucion.addEventListener("click", function () {
+  Swal.fire({
+    title: "Dar Devolución",
+    input: "textarea",
+    inputPlaceholder: "Ingrese su devolución...",
+    showCancelButton: true,
+    confirmButtonText: "Enviar",
+    cancelButtonText: "Cancelar",
+    preConfirm: (devolucion) => {
+      if (!devolucion) {
+        Swal.showValidationMessage("Debe ingresar una devolución");
+      } else {
+        return devolucion;
+      }
+    }
+  }).then((result) => {
+    if (result.isConfirmed) {
+      const devolucion = result.value;
+      guardarDevolucion(devolucion);
+    }
+  });
+});
+
+
+function guardarDevolucion(devolucion) {
+  const performanceActual = performancesDanzaContemporanea[performancesDanzaContemporanea.length - 1];
+  const devolucionGuardada = {
+    performance: performanceActual.nombre,
+    texto: devolucion,
+    videoLink: performanceActual.youtubeLink,
+    userName: `${localStorage.getItem("firstName")} ${localStorage.getItem("lastName")}`
+  };
+  devoluciones.push(devolucionGuardada);
+  actualizarContadorDevoluciones();
+
+  Swal.fire({
+    icon: "success",
+    title: "Devolución exitosa",
+    text: "Tu devolución ha sido guardada exitosamente",
+    confirmButtonText: "Aceptar"
+  });
 }
